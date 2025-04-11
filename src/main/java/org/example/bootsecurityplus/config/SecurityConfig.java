95ed3f30347442a854304b78a41730d7ddc2442e
qus0in <qus0in@gmail.com>
2025-04-11 11:31:52 +0900

    시큐리티 체험

--- src/main/java/org/example/bootsecurityplus/config/SecurityConfig.java
+++ src/main/java/org/example/bootsecurityplus/config/SecurityConfig.java
@@ -2,7 +2,13 @@ package org.example.bootsecurityplus.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,6 +16,30 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll() // '/'는 모두에게 허용이 되어 있다
+                        .requestMatchers("/admin").hasRole("ADMIN") // ADMIN
+                        .requestMatchers("/user").hasRole("USER") // USER
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults()) // 기본을 쓰자
+                .logout(Customizer.withDefaults()); // 기본을 쓰자
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
--- src/main/java/org/example/bootsecurityplus/controller/IndexController.java
+++ src/main/java/org/example/bootsecurityplus/controller/IndexController.java
@@ -8,7 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("msg", "거짓말쟁이");
+        model.addAttribute("msg", "로그인이 아닙니다");
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("msg", "어드민입니다");
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model) {
+        model.addAttribute("msg", "유저입니다");
         return "index";
     }
 }
